e5e46f9536f326b4c2148880a53e9593fed0be09
Aleksandrs Belkins <aleksandrs.belkins@gmail.com>
2021-11-01 14:52:09 +0200

    added read-more/less buttons

---
+++ public/js/blog.js
@@ -0,0 +1,27 @@
+let textElements = document.querySelectorAll('.blog__item-text')
+let maxlength = 200;
+
+textElements.forEach((item) => {
+    let text = item.textContent.trim();
+    let formatedText = text.slice(0, maxlength - 1) + '…';
+
+    if (text.length > maxlength) {
+        item.textContent = formatedText;
+        let linkAdd = document.createElement('a');
+        linkAdd.textContent = 'Read more';
+        linkAdd.classList.add('text-link')
+        item.append(linkAdd);
+
+        linkAdd.addEventListener('click', () => {
+            linkAdd.parentNode.textContent = text;
+            let linkHide = document.createElement('a');
+            linkHide.textContent = 'Read less';
+            linkHide.classList.add('text-link')
+            item.append(linkHide);
+            linkHide.addEventListener('click', () => {
+                linkHide.parentNode.innerHTML = formatedText;
+                item.append(linkAdd);
+            })
+        })
+    }
+})
